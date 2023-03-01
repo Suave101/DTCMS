a213f4ccc1749f84e24155821fb8dec427f99c68
Suave101 <57422635+Suave101@users.noreply.github.com>
2023-03-01 11:20:24 -0600

    Remove Redundant Text

--- eventManager.php
+++ eventManager.php
@@ -11,6 +11,5 @@
         <a href="currentScores.php">Current Scores</a>
         <a href="login.php"><?php if (isset($_SESSION["authenticated"]) and $_SESSION["authenticated"] === true) {echo "Account Menu";} else {echo "Login";}?></a>
     </div>
-    <p>Welcome to the DTC Scoring System. If you are a team member, coach, or spectator, you can see the current scores on the current scores page. If you are a judge or administrator, you can access your tools via the login page.</p>
 </body>
 </html>
